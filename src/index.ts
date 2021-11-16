7f7054f653e5d2ba01fb16d3318fe47fe786b187
Maxim Zass <fantom.zms14@gmail.com>
2021-11-16 15:55:23 +0300

    fixes

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express()
 
 app.use(cors({
   credentials: true,
-  origin: [process.env.CLIENT_URL || '', process.env.ADMIN_URL || ''] 
+  origin: [process.env.CLIENT_URL || '', process.env.ADMIN_URL || '']
 }))
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
